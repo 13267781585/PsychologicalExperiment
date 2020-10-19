5b4221c518ebe5638e8df153b5b1a399dccec081
root1 <root1@.com>
2020-10-19 19:54:31 +0800

    logging init

--- src/main/java/jianxin/psyExperiment/controller/Impl/UserControllerImpl.java
+++ src/main/java/jianxin/psyExperiment/controller/Impl/UserControllerImpl.java
@@ -3,6 +3,8 @@ package jianxin.psyExperiment.controller.Impl;
 import jianxin.psyExperiment.controller.UserController;
 import jianxin.psyExperiment.entity.User;
 import jianxin.psyExperiment.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,11 +13,14 @@ import java.util.List;
 
 @RestController
 public class UserControllerImpl implements UserController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserControllerImpl.class);
+
     @Autowired
     private UserService userService;
 
     @RequestMapping("/findAll")
     public List<User> findAllUser() throws Exception {
+        LOGGER.info("==================println======================");
         return userService.findAllUser();
     }
 }
